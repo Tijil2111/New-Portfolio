b04183e8a59ea462d03e80c9c129ef1d7b8b29ee
Tijil Agrawal <77985046+Tijil2111@users.noreply.github.com>
2022-03-30 18:59:43 +0530

    Changes

--- src/components/404.jsx
+++ src/components/404.jsx
@@ -4,8 +4,10 @@ import React from "react";
 import { FaAngleLeft } from "react-icons/fa";
 import { Outlet, Link } from "react-router-dom";
 export const ErrorPage = () => {
+  // getting the url of the wrong page, so i can show it
   const ddd = window.location.href;
   console.log(ddd);
+
   return (
     <div>
       <div className="pt-40 pl-32 text-7xl text-indigo-600">Oops !</div>
